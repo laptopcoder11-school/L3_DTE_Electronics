74b4a6d709bb93cac322803088555b0802fa0f1a
laptopcoder11-school <106130005+laptopcoder11-school@users.noreply.github.com>
2024-10-21 12:10:58 +1100

    Code Version 3
    (forgot to upload version 2)
    v2 : Added 2 line alphanumeric display to show sensor infomation
    v3 : Added interupts for display backlight

--- dte_greenhouse_moniter_program.ino
+++ dte_greenhouse_moniter_program.ino
@@ -1,7 +1,9 @@
 /*
  * This program runs the control box for the greenhouse moniter 
  * and toggles as reley when specific conditions are met
- * version 1 -> reads in the temp and humidity sensor and activates a relay when above a threshhold
+ * version 3 -> reads in the dht sensor and
+ * displays the sensor readings on a simple 2 line Alphanumeric LED display
+ * Also uses an interupt to toggle the display backlight when a button is pressed
  */
 
 //include the libarys that are required 
@@ -9,26 +11,59 @@
 #include <LiquidCrystal_I2C.h> //led display
 #include <DHT11.h> //temp and humidity sensor
 
+// set up the LCD display to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,20,4);  
+
 //set the port for the temp and humidity sensor
 DHT11 dht11(2);
 
+//interupt
+const int interruptPin = 3;
+bool ledbacklight = LOW;
+
+
 void setup()
 {
+   // initialize the lcd display
+  lcd.init();                      
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  
   //set the serial port for debugging
   Serial.begin(9600);
   pinMode(8, OUTPUT); //relay output
+
+  //atatch an interupt for ottggling backlight
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), toggleDisplayBacklight, RISING);
 }
+
+void toggleDisplayBacklight()
+{
+ledbacklight = !ledbacklight;
+Serial.println("Interupt");
+}
+
 
 void loop()
 {
     //declare constants and variables
     int temperature = 0;
     int humidity = 0;
-    const int temperatureLimit = 55; //placeholder limit value
+    const int temperatureLimit = 42; //placeholder limit value
 
     //Read the temperature and humidity
     int result = dht11.readTemperatureHumidity(temperature, humidity);
 
+    //change the backlight
+    if (ledbacklight)
+    {
+    lcd.backlight();
+    }else
+    {
+    lcd.noBacklight();
+    }
+  
     // Check the results of the readings.
     if (result == 0) {
         //serial print the temp and humidity for debugging
@@ -37,6 +72,16 @@ void loop()
         Serial.print(" °C\tHumidity: ");
         Serial.print(humidity);
         Serial.println(" %");
+
+         //draw the output on the LCD
+         lcd.setCursor(0,0);
+         lcd.print("Temp : ");
+         lcd.print(temperature);
+         lcd.print("°C");
+         lcd.setCursor(0,1);
+         lcd.print("Humid : ");
+         lcd.print(humidity);
+         lcd.print("%");
         
         if (humidity > temperatureLimit)
         {
